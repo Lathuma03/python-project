357007b485d6dc3826baa3d7405b48f78306aa6c
Lathuma03 <elangolathangi@gmail.com>
2024-07-15 00:11:10 +0530

    intern project

--- bmi.py
+++ bmi.py
@@ -1,55 +1,59 @@
 import streamlit as st
-st.set_page_config(page_title="BMI CALCULATOR",page_icon=":white_flower:",layout="wide")
+
+st.set_page_config(page_title="BMI CALCULATOR", page_icon=":white_flower:", layout="wide")
+
 st.title("BMI CALCULATOR")
 st.header("A webpage to calculate Body Mass Index of a Person")
+
 name = st.text_input("Enter Your name", "")
-if(st.button('Submit')):
-	result = name.title()
-	st.success(result)
-gender= st.radio("what is your Gender?",('Female','Male','Transgender'))
-if(gender=='Female'):
-    st.success("Female")
-elif(gender=='Male'):
-    st.success("Male")
+if st.button('Submit Name'):
+    result = name.title()
+    st.success(f"Hello, {result}!")
+
+gender = st.radio("What is your Gender?", ('Female', 'Male', 'Transgender'))
+st.success(gender)
+
+age = st.number_input("Enter your Age:", min_value=0, max_value=150)
+add = st.text_input("Enter your Address:")
+
+hobbies = []
+if st.checkbox("Dancing"):
+    hobbies.append("Dancing")
+if st.checkbox("Music"):
+    hobbies.append("Music")
+if st.checkbox("Watching Movies"):
+    hobbies.append("Watching Movies")
+if st.checkbox("Exercise"):
+    hobbies.append("Exercise")
+if st.checkbox("Gaming"):
+    hobbies.append("Gaming")
+
+weight = st.number_input("Enter your weight (in kgs)", min_value=0.0)
+status = st.radio('Select your height format:', ('cms', 'meters', 'feet'))
+
+if status == 'cms':
+    height = st.number_input('Centimeters', min_value=0.0)
+    height_in_meters = height / 100
+elif status == 'meters':
+    height = st.number_input('Meters', min_value=0.0)
+    height_in_meters = height
 else:
-    st.success("Transgender")
-age=st.number_input("Enter your Age:")
-add=st.text_input("Enter your Address:")
-hob1=st.checkbox("Dancing")
-hob2=st.checkbox("Music")
-hob3=st.checkbox("Watching Movies")
-hob4=st.checkbox("Exercise")
-hob5=st.checkbox("Gaming")
-weight = st.number_input("Enter your weight (in kgs)")
-status = st.radio('Select your height format: ',
-                  ('cms', 'meters', 'feet'))
-if(status == 'cms'):
-    height = st.number_input('Centimeters')
-    try:
-        bmi = weight / ((height/100)**2)
-    except:
-        st.text("Enter some value of height")
-elif(status == 'meters'):
-    height = st.number_input('Meters')
-    try:
-        bmi = weight / (height ** 2)
-    except:
-        st.text("Enter some value of height")
-else:
-    height = st.number_input('Feet')
-    try:
-        bmi = weight / (((height/3.28))**2)
-    except:
-        st.text("Enter some value of height")
-if(st.button('Calculate BMI')):
-    st.text("Your BMI Index is {}.".format(bmi))
-    if(bmi < 16):
-        st.error("You are Extremely Underweight")
-    elif(bmi >= 16 and bmi < 18.5):
-        st.warning("You are Underweight")
-    elif(bmi >= 18.5 and bmi < 25):
-        st.success("Healthy")
-    elif(bmi >= 25 and bmi < 30):
-        st.warning("Overweight")
-    elif(bmi >= 30):
-        st.error("Extremely Overweight")
+    height = st.number_input('Feet', min_value=0.0)
+    height_in_meters = height / 3.28
+
+if st.button('Calculate BMI'):
+    if height_in_meters > 0:
+        bmi = weight / (height_in_meters ** 2)
+        st.text(f"Your BMI Index is {bmi:.2f}.")
+        if bmi < 16:
+            st.error("You are Extremely Underweight")
+        elif 16 <= bmi < 18.5:
+            st.warning("You are Underweight")
+        elif 18.5 <= bmi < 25:
+            st.success("Healthy")
+        elif 25 <= bmi < 30:
+            st.warning("Overweight")
+        else:
+            st.error("Extremely Overweight")
+    else:
+        st.error("Please enter a valid height.")
